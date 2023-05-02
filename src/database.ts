ab9ce2626b635fe6cd2afa2b442e98182c5ef286
Facundo <facuhannoch@gmail.com>
2023-05-02 01:20:25 -0300

    Minor fix

--- src/database.ts
+++ src/database.ts
@@ -3,17 +3,14 @@ import { MongoClient } from 'mongodb';
 const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 const dbName = process.env.MONGODB_DB || 'your_db_name';
 
-let cachedClient: MongoClient = null;
+let cachedClient: MongoClient | null = null;
 
 export async function connectToDatabase() {
   if (cachedClient) {
     return { client: cachedClient, dbName };
   }
 
-  const client = await MongoClient.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  const client = await MongoClient.connect(uri);
 
   cachedClient = client;
 
